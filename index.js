20d65487d1ddae3c0149003f1d7311aceb3fa65f
Aymen <aymenbouazra994@gmail.com>
2024-02-24 00:16:37 +0100

    vercel

--- index.js
+++ index.js
@@ -7,11 +7,26 @@ const passport = require('passport');
 const cors = require('cors');
 const session = require('express-session');
 const port = process.env.PORT || 4000
+
+const corsOptions = {
+    origin: 'https://motion-agency.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
 require('dotenv').config()
 require('./database/db')
 require('./commun/initScript')
 require('./middlewares/Bearer')
-app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://motion-agency.vercel.app');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.options('*', cors(corsOptions));
+
 app.use(morgan('dev'))
 app.use(express.urlencoded({ limit: '100mb', extended: true }))
 app.use(express.json({ limit: '100mb' }))
