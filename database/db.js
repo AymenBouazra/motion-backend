13336930a7be2f67c4c5312a8a662b0e6c8a39ac
Aymen <aymenbouazra994@gmail.com>
2024-02-24 01:00:44 +0100

    changed db

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/Motion-agency')
+const db_test = 'mongodb://localhost:27017/test';
+const db_dev = 'mongodb+srv://motion:azerty123@mydatabase.8vzuo.mongodb.net/motion'
+mongoose.connect(db_dev)
     .then(() => {
         console.log('Database connected successfully');
     })
